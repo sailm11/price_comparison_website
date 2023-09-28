2021bcf2beab7c2f9d238ba97aa9927781cf922f
Kundan Kumavat <kundankumawat003@gmail.com>
2023-09-28 12:15:22 +0530

    initial commit

---
+++ flipkart.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+phone_name = []
+phone_prices = []
+
+page_number = input("Enter the number of pages: ")
+for i in range(1, int(page_number)+1):
+    url="https://www.flipkart.com/search?q=iphone+15&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=off&as=off&page="+str(i)
+    req = requests.get(url)
+    content= BeautifulSoup(req.content, 'html.parser')
+#     print(content)
+    name= content.find_all('div', {"class": '_4rR01T'})
+    price = content.find_all('div', {"class": "_30jeq3 _1_WHN1"})
+    print(len(name))
+    
+    for i in name:
+        phone_name.append(i.text)
+    for i in price:
+        phone_prices.append(i.text)
+
+for i in phone_name:
+    print(i)
+
+for i in phone_prices:
+    print(i)
+
+data={'phone name': phone_name, "phone prices": phone_prices}
+df=pd.DataFrame(data)
+print(df)
